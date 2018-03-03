ffab2f6a726cc48ab76f38c4332622f5ffbece8b
Elijah <elijahsharper1@gmail.com>
2018-03-04 01:09:21 +0300

    css set up

--- app.js
+++ app.js
@@ -14,5 +14,14 @@ const starsTotal = 5;
 document.addEventListener('DOMContentLoaded', getRatings);
 
 function getRatings() {
-  
+  for (let rating in ratings) {
+    // Get percentage
+    const starPercentage = (ratings[rating] / starsTotal) * 100
+
+    // Round to nearest 10
+    const starPercentageRounded = `${Math.round(starPercentage / 10) * 10}%`;
+
+    // Set width of stars-inner to percentage
+    document.querySelector(`.${rating} .stars-inner`).style.width = starPercentageRounded;
+  }
 }
