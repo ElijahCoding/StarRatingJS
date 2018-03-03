82d0e22c44893929a7b643e136740d17085dde33
Elijah <elijahsharper1@gmail.com>
2018-03-04 01:00:18 +0300

    Init js

---
+++ app.js
@@ -0,0 +1,18 @@
+// Initial Ratings
+ const ratings = {
+ sony: 4.7,
+ samsung: 3.4,
+ vizio: 2.3,
+ panasonic: 3.6,
+ phillips: 4.1
+}
+
+// Total Stars
+const starsTotal = 5;
+
+// Run getRatings when DOM loads
+document.addEventListener('DOMContentLoaded', getRatings);
+
+function getRatings() {
+  
+}
